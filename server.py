855da1a77d5debc28b064a3aea87526f08f1439f
ewartboonstra <e.boonstra@hotmail.nl>
2016-11-24 13:28:29 +0100

    added b64 encode

--- server.py
+++ server.py
@@ -1,28 +1,52 @@
 import zmq
+import cv2
+import base64
+
+
+# source image using imread
+def converttobase64(img):
+    print "test"
+    # cnt = cv2.imwrite("kenteken.png", img)
+    cnt = cv2.imencode('.png',img)[1]
+    return base64.encodestring(cnt)
+
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-connectionString = "145.44.81.71:9023"
+connectionString = "127.0.0.1:9023"
 
 socket.bind("tcp://" + connectionString)
 print "Server started on %s" % connectionString
+print "waiting for client connection"
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv()
-    print "message received: %s" % message
-    socket.send_string(message)
+#first wait for hello message
+message = socket.recv()
+print "message received: %s" % message
+# socket.send_string(message)
 
 
-def sentImage():
-    print "test"
+print "send image test"
+# source image
+img = cv2.imread('img/kenteken-apart.png')
+b64 = converttobase64(img)
+print b64
+socket.send_string(b64)
+
+
+def debug():
+    while True:
+        # Wait for next request from client
+        message = socket.recv()
+        print "message received: %s" % message
+        socket.send_string(message)
+
+
 
 
 def waitforanswer():
     while True:
         #  Wait for next request from client
         message = socket.recv()
-
         return message
 
 # # Define a function for the thread
