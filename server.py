518cc6274233a74a10c21d397b41eb8d1b29ff24
ewartboonstra <e.boonstra@hotmail.nl>
2016-11-29 10:50:46 +0100

    Server connection finalized

--- server.py
+++ server.py
@@ -3,66 +3,34 @@ import cv2
 import base64
 
 
-# source image using imread
-def converttobase64(img):
-    print "test"
-    # cnt = cv2.imwrite("kenteken.png", img)
-    cnt = cv2.imencode('.png',img)[1]
+# Convert to base64 image for easy transferring
+def convertToBase64(img):
+    cnt = cv2.imencode('.png', img)[1]
     return base64.encodestring(cnt)
 
 
+# open connection to c# server
+def OpenConnection():
+    connectionString = "127.0.0.1:9023"
+
+    socket.bind("tcp://" + connectionString)
+    print "Connected to server on %s" % connectionString
+
+
+# Close server connection
+def closeConnection():
+    socket.close()
+    print "Connection closed"
+
+
+# Send image of kenteken to c# server
+def sendImg(img):
+    # debug image
+    # img = cv2.imread('img/kenteken-apart.png')
+    b64 = convertToBase64(img)
+    socket.send_string(b64)
+    print "Image sent"
+
+
 context = zmq.Context()
-socket = context.socket(zmq.REP)
-connectionString = "127.0.0.1:9023"
-
-socket.bind("tcp://" + connectionString)
-print "Server started on %s" % connectionString
-print "waiting for client connection"
-
-#first wait for hello message
-message = socket.recv()
-print "message received: %s" % message
-# socket.send_string(message)
-
-
-print "send image test"
-# source image
-img = cv2.imread('img/kenteken-apart.png')
-b64 = converttobase64(img)
-print b64
-socket.send_string(b64)
-
-
-def debug():
-    while True:
-        # Wait for next request from client
-        message = socket.recv()
-        print "message received: %s" % message
-        socket.send_string(message)
-
-
-
-
-def waitforanswer():
-    while True:
-        #  Wait for next request from client
-        message = socket.recv()
-        return message
-
-# # Define a function for the thread
-# def print_time(threadName, delay):
-#     count = 0
-#     while count < 5:
-#         time.sleep(delay)
-#         count += 1
-#         print "%s: %s" % (threadName, time.ctime(time.time()))
-#
-#
-# # Create two threads as follows
-# try:
-#     thread.start_new_thread(server, ())
-# except:
-#     print "Error: unable to start thread"
-#
-# while 1:
-#     pass
+socket = context.socket(zmq.PAIR)
