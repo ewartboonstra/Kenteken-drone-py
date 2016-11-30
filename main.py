e027a865cfbb40d0c15be915b53a9560d9c151fd
Vincent Kloppenburg <vincentkloppenburg1996@live.nl>
2016-11-30 16:46:09 +0100

    small refractor with a small improvement in performance

--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def distance_between_points(p1, p2):
 def ratio(line1, line2):
     return line2 / line1
 
-def find_rectangle(img, thresh):
-    # 1
+#finds all the contours and returns them with thier hieracrhy
+def find_contours(img, thresh):
+    #1
     src = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
     # 2
     src = cv2.GaussianBlur(src, (5, 5), 0)
@@ -29,8 +30,11 @@ def find_rectangle(img, thresh):
 
     # 5
     dst, conts, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # 6
-    recs = []
+
+    return conts, hierarchy
+
+def find_squares(conts):
+    squares = []
     current_index = 0
     for cnt in conts:
         cnt_len = cv2.arcLength(cnt, True)
@@ -56,17 +60,33 @@ def find_rectangle(img, thresh):
             len4 = distance_between_points(cnt[3][0],cnt[1][0])
             #check if ratio is licence plate like
             if (ratio(len1, len2) > 3.0 and ratio(len1, len2) < 8.0) and (ratio(len3, len4) > 3.0 and ratio(len3, len4) < 8.0):
-                j = 0
-                for hier in hierarchy[0]:
-                    #check if rectangle has other shapes inside it (posibly letters)
-                    if hier[3] == current_index:
-                        #removing potential noice
-                        if cv2.contourArea(conts[j]) > 1000:
-                            print 'found plate-like'
-                            recs.append(cnt)
-                    j = j + 1
+                square = current_index, cnt
+                squares.append(square)
         current_index = current_index + 1
+    return squares
+
+def find_potential_plate(squares, hierarchy, conts):
+    recs = []
+    for square in squares:
+        j = 0
+        print square[1]
+        for hier in hierarchy[0]:
+        #check if rectangle has other shapes inside it (posibly letters)
+            if hier[3] == square[0]:
+                #removing potential noice
+                if cv2.contourArea(conts[j]) > 1000:
+                    recs.append(square[1])
+            j = j + 1
     return recs
+
+def find_rectangle(img, thresh):
+    # 1
+    conts, hierarchy = find_contours(img, thresh)
+    squares = find_squares(conts)
+    plates = find_potential_plate(squares, hierarchy, conts)
+    return plates
+    # 6
+
 
     '''
     Stappenplan herkennen rechthoek
@@ -86,7 +106,6 @@ def show_img():
         for x in xrange(15, 240, 15):
             plates.append(find_rectangle(fn, x))
         img = cv2.imread(fn, cv2.IMREAD_COLOR)
-        print plates
         for plate in plates:
             cv2.drawContours(img, plate, -1, (255, 0, 255), 6 )
         cv2.namedWindow('squares', cv2.WINDOW_NORMAL)
