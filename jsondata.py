02f32fe8a04bee4d2a90a7654e221049d8939e4d
Matthijs Timmerman <matthijs837@live.nl>
2017-01-11 14:29:22 +0100

    implemented feedback from Ewart

--- jsondata.py
+++ jsondata.py
@@ -4,18 +4,23 @@ import base64
 import time
 import datetime
 
-# getJson() methode neemt als argument filepath naar een afbeelding. Vervolgens wordt dit bestand omgezet in
+# convertToJson() methode neemt als argument filepath naar een afbeelding. Vervolgens wordt dit bestand omgezet in
 # een base64 string, die samen met een timestamp en GPS coordinaten worden toegevoegd aan een json bestand.
 # dit .json bestand wordt nu returned.
 
 class Json(object):
-    dateTimeNotation = '%Y-%m-%d %H:%M:%S' # deze notatie wordt gebruikt om een timestamp mee te geven.
+    dateTimeNotation = '%Y-%m-%d %H:%M:%S'
+    # deze notatie wordt gebruikt om een timestamp mee te geven.
+
+    # Deze methode wordt gebruikt om een image naar base64 string om te zetten.
+    # speciale methode voor convertToBase64 is een designkeuze. Eventueel later uitbreidbaar en 'with' statement
+    # werkt als een try/catch statement.
     def convertToBase64(self, imagePath):  # convert image file naar base64 string
         with open(imagePath, "rb") as imageFile:
             string = base64.b64encode(imageFile.read())
             return string
 
-    def getJson(self, imagePath):
+    def convertToJson(self, imagePath):
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime(self.dateTimeNotation) # maakt een timestamp
         base64string = self.convertToBase64(imagePath)
